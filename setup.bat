e64a429d75dc06707ee01107a8b4c9442a5d701b
DaveGlorbus <149727498+DaveGlorbus@users.noreply.github.com>
2025-01-04 20:06:03 +0000

    Make it just generate teams and count instances

--- setup.bat
+++ setup.bat
@@ -1,5 +1,2 @@
 set PYTHONPATH=%PYTHONPATH%;%cd%
-py ./BattleFactoryBuddy/StaticDataGenerator.py
-py -m venv .venv
-.venv\Scripts\pip install -r requirements.txt
-.venv\Scripts\python manage.py runserver
+py ./BattleFactoryBuddy/TeamUniformityCalc.py
