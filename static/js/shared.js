a2eb70270f3ada5a3d8cada956b75a73ece85b84
William Jackson <will-jj@users.noreply.github.com>
2025-01-11 12:08:52 +0000

    Initial addition of share button to teambuilder

--- static/js/shared.js
+++ static/js/shared.js
@@ -14,3 +14,37 @@ function setFormFromParams(urlParams) {
     }
   });
 }
+
+async function createAndCopyShareLink(initialParams = null, includePathName = false) {
+    let params = new URLSearchParams();
+
+    if (initialParams){
+        params = initialParams;
+    }
+
+    const formElements = document.querySelectorAll('.form-select, .form-check-input, .form-control');
+
+
+    formElements.forEach(element => {
+        if (element.type === 'checkbox') {
+            params.append(element.name, element.checked ? 'on' : 'off');
+        }
+        else {
+            params.append(element.name, element.value);
+        }
+    });
+
+    params.append('Calc', 1);
+
+    let pathname = "/";
+    if (includePathName){
+      pathname = `${window.location.pathname}`
+    }
+
+    const url = `${window.location.protocol}//${window.location.host}${pathname}?${params.toString()}`;
+    try {
+        await navigator.clipboard.writeText(url);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
